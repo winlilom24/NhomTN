939c33700038757e1e012e0448d6d761f4debc4e
winlilom <ducnhat2425@gmail.com>
2025-05-26 20:10:52 +0700

    xoa //

--- views/mailAdminAcount.php
+++ views/mailAdminAcount.php
@@ -1,5 +1,4 @@
 <?php
-// Thông tin cấu hình email tách riêng (bạn nên tạo file .env hoặc config riêng)
 $mailUser = 'trananhvu1412@gmail.com';
-$mailPass = 'ijry rkev elkd difu'; // Tốt nhất lưu file config bên ngoài public_html
+$mailPass = 'lkxm tjwn zdqj jgwn'; 
 ?>
