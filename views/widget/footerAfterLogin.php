fa39f500848ad77eb764a610bf6ea91106f62033
hoangkq157 <hoang1234098@gmail.com>
2025-05-29 20:04:43 +0700

    Thêm footer sau khi đăng nhập

---
+++ views/widget/footerAfterLogin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chủ - Hệ Thống Thi Trắc Nghiệm</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="../style/css/homepage1.css">
+</head>
+<div class="footer">
+        <p>Bình tĩnh - Tự tin - Chiến thắng!
+            Kiến thức đã có, giờ chỉ cần thể hiện hết mình. Chúc bạn đạt kết quả như mong đợi và còn hơn thế nữa!" ✨</p>
+</div>
